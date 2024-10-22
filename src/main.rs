2e1caa4aa94bc08bf67dcbfdb9f3c3e8d6e41348
Saksham Dev <105414841+sakshamdev123@users.noreply.github.com>
2024-10-22 14:03:56 +0530

    Update port and static file

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use actix_web::{App, HttpServer, web::Data};
 use actix_files::Files;
+use actix_web::{web::Data, App, HttpServer};
 
 mod routes;
 use routes::*;
@@ -13,6 +13,10 @@ async fn main() -> std::io::Result<()> {
         .await
         .expect("Failed to connect to database");
     println!("Database connection established");
+    let port = std::env::var("PORT")
+        .unwrap_or_else(|_| "3000".to_string()) // Default to 3000 if PORT is not set
+        .parse()
+        .expect("PORT must be a number");
     let server = HttpServer::new(move || {
         App::new()
             .app_data(Data::new(database.clone()))
@@ -25,10 +29,10 @@ async fn main() -> std::io::Result<()> {
             .service(update_todo_description)
             .service(mark_todo_completed)
             .service(delete_todo)
-            .service(Files::new("/", "./static").show_files_listing())
+            .service(Files::new("/", "./static").index_file("index.html"))
     })
-    .bind(("0.0.0.0", 8080))?
+    .bind(("0.0.0.0", port))?
     .run();
-    println!("Server Running at port 8080");
+    println!("Server Running at port {}", port);
     server.await
 }
